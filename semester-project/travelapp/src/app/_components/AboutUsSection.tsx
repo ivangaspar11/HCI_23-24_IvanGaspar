4aec140733f0987e595fe006fee49e2deed18b08
ivangaspar11 <ivan.gaspar11@gmail.com>
2023-12-06 17:06:59 +0100

    bugfix

--- semester-project/travelapp/src/app/_components/AboutUsSection.tsx
+++ semester-project/travelapp/src/app/_components/AboutUsSection.tsx
@@ -6,11 +6,11 @@ const AboutUsSection: React.FC = () => {
   return (
     <section className="py-12 bg-gray-100">
       <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold mb-6">About Us</h2>
+        <h2 className="text-4xl font-bold mb-6">Discover Your Journey With Us</h2>
         <p className="text-gray-700 text-lg leading-relaxed mb-8">
-          At [Your Travel Agency], we embark on a mission to redefine your travel experience. Our dedication to excellence and personalized service ensures that each journey is meticulously crafted to exceed expectations. As passionate travelers ourselves, we specialize in curating unforgettable adventures tailored to your unique preferences. Our experienced team is committed to turning your travel dreams into seamless and enriching realities. Whether you crave a serene beach escape, an exhilarating mountain expedition, or a culturally immersive exploration, we&apos;re here to guide you through extraordinary journeys. Join us and let&apos;s create travel memories that last a lifetime.
+          At ITA, we embark on a mission to redefine your travel experience. Our dedication to excellence and personalized service ensures that each journey is meticulously crafted to exceed expectations. As passionate travelers ourselves, we specialize in curating unforgettable adventures tailored to your unique preferences. Our experienced team is committed to turning your travel dreams into seamless and enriching realities. Whether you crave a serene beach escape, an exhilarating mountain expedition, or a culturally immersive exploration, we&apos;re here to guide you through extraordinary journeys. Join us and let&apos;s create travel memories that last a lifetime.
         </p>
-        <button className="bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-700 text-lg">
+        <button className="bg-gray-500 text-white px-6 py-3 rounded-full hover:bg-gray-700 text-lg">
           Learn More
         </button>
       </div>
